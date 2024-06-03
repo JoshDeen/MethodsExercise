cb0f7d0cccf5fb001869fc6a9c3e6ca3df559072
Joshua Wilson <joshdeenwilson@gmail.com>
2024-06-03 12:07:00 -0400

    Excercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello - What is your first name?");   
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}.  What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color}  Is an awesome color!  What is your favirite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great!  Now what is your favorite band?");
+            var band = Console.ReadLine();
+
         }
+
+
     }
+    
 }
